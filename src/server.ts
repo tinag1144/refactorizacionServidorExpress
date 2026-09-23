d5f195fcae1ff86a0818a52308c1dd518ce4dc10
unknown <sogazelaznog@gmail.com>
2026-09-23 16:07:42 -0300

    Me olvide de añadir el server modificado

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express, { Application } from "express"
 import mongoose from "mongoose";
 import { EmployeeRoutes } from "./routes/employees.routes.js";
 import { errorHandler } from "./errorHandler/errorHandler.js";
+import { connectDatabase } from "./config/database.js";
 
 class Server {
 
@@ -28,35 +29,25 @@ class Server {
 
     
 
-    async dbConnect() {
-    try {
-        await mongoose.connect(
-            process.env.MONGO_URI || "mongodb://localhost:27017/employees_db"
-        );
-
-        console.log("Base de datos conectada");
-
-    } catch (error) {
-        console.error("Error al conectar con la base de datos", error);
-        process.exit(1);
-    }
-}
-
-
     listen() {
         const PORT = process.env.PORT || 3000;
-
+        
         this.app.listen(PORT, () => {
             console.log(`Servidor corriendo en el puerto ${PORT}`);
         });
     }
+    
 }
+
+
+
+
 
 const server = new Server();
 
 server.middlewares();
 server.routes();
 
-server.dbConnect().then(() => {
+connectDatabase().then(() => {
     server.listen();
 });
